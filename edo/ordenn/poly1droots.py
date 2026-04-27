e0f067959bf76521101b8c1d38140d76e8f1c892
claudiajimenachaconbarrera-source <claudiajimenachaconbarrera@esiaticipnct.com>
2026-04-27 07:43:15 -0600

    Update poly1droots.py

--- edo/ordenn/poly1droots.py
+++ edo/ordenn/poly1droots.py
@@ -1,8 +1,14 @@
-from numpy import *
-p=poly1d([3,0,0,1,1])
-print(p)
-print(p.r)
-pp=poly1d([1, -2021, 0, 1, 1])
-print(p)
-print(p.r)
+import numpy as np
+
+# Definimos los coeficientes: [23, 1, 0, 0, 1]
+# Representan: 23m^4 + 1m^3 + 0m^2 + 0m + 1
+coeficientes = [23, 1, 0, 0, 1]
+
+# Calculamos las raíces
+raices = np.roots(coeficientes)
+
+print("Las raíces de la ecuación son:")
+for i, r in enumerate(raices, 1):
+    print(f"m{i} = {r}")
+
 
